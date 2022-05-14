693e560b0301b96fa67007c9f8f709db857d9540
Josh Waalkes <josh.waalkes21@gmail.com>
2022-05-14 14:32:03 -0600

    working on passing params to server from client

--- server/routes/question.js
+++ server/routes/question.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
+const axios =  require('axios');
+const APIurl = 'https://app-hrsei-api.herokuapp.com/api/fec2/:hr-rpp';
+const config = require('/Users/josh/HRWork/Senior/atelier/config.js');
 
 router.get('/', (req, res) => {
-
+  let product_id = req.params;
+  console.log(product_id);
+  options = {url: `${APIurl}/qa/quesitons`,
+    headers: {
+      'Authorization': `token ${config.TOKEN}`
+    },
+    params: {product_id: product_id}
+}
+  axios(options)
+    .then( (APIres) => {
+      res.send(APIres.data);
+    })
+    .catch( (err) => {
+      console.log('error getting question data in router', err);
+    })
 })
 
 module.exports = router;
