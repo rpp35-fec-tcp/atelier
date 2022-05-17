b78b5b11cc6f0c8ae981d0bb566fd9adba966426
ysyanshen <ysyanshen1987@gmail.com>
2022-05-16 18:09:02 -0700

    add some test cases

--- testing/relatedTest/carousel.test.js
+++ testing/relatedTest/carousel.test.js
@@ -1,34 +1,7 @@
-import { render } from '@testing-library/react';
 import React from 'react';
 import SimpleCarousel from '../../client/src/components/relatedItems&comparison/carousel.jsx';
-import Card from '../../client/src/components/relatedItems&comparison/card.jsx';
 import ReactShallowRenderer from 'react-test-renderer/shallow';
-
-var currentProductInfo = {
-  "id": 71704,
-  "campus": "hr-rpp",
-  "name": "YEasy 350",
-  "slogan": "Just jumped over jumpman",
-  "description": "These stretchy knit shoes show off asymmetrical lacing and a big sculpted rubber midsole. In a nod to adidas soccer heritage.",
-  "category": "Kicks",
-  "default_price": "450.00",
-  "created_at": "2022-05-11T19:38:15.373Z",
-  "updated_at": "2022-05-11T19:38:15.373Z",
-  "features": [
-      {
-          "feature": "Sole",
-          "value": "Rubber"
-      },
-      {
-          "feature": "Material",
-          "value": "FullControlSkin"
-      },
-      {
-          "feature": "Stitching",
-          "value": "Double Stitch"
-      }
-  ]
-};
+import fakeProductInfos from './fakeData.js';
 
 describe('SimpleCarousal', function() {
 
@@ -37,15 +10,25 @@ describe('SimpleCarousal', function() {
     //shallow(<SimpleCarousel />);
   });
 
-  it('should render one `Card` when given one video', function() {
+  it('should render one `Card` when given array with one related product', function() {
 
     var shallowRenderer = new ReactShallowRenderer();
-    var oneFakeRelatedProducts = [71699];
+    var fakeRelatedProducts = [71699];
     shallowRenderer.render(
-      <SimpleCarousel relatedProducts={oneFakeRelatedProducts} currentProductInfo={currentProductInfo} />
+      <SimpleCarousel relatedProducts={fakeRelatedProducts} currentProductInfo={fakeProductInfos[0]} />
     );
     var simpleCarousel = shallowRenderer.getRenderOutput();
     expect(simpleCarousel.props.children.length).toBe(1);
   });
+
+  it ('should render multiple `Card` when given array with multiple related products', function () {
+    var shallowRenderer = new ReactShallowRenderer();
+    var fakeRelatedProducts = [71699, 71701, 71702];
+    shallowRenderer.render(
+      <SimpleCarousel relatedProducts={fakeRelatedProducts} currentProductInfo={fakeProductInfos[0]} />
+    )
+    var simpleCarousel = shallowRenderer.getRenderOutput();
+    expect(simpleCarousel.props.children.length).toBe(3);
+  })
 
 });
