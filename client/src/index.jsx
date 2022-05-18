a7e42be621f34c25b6eb0160de72ac8568e485f2
kinyipchien <56460213+kinyipchien@users.noreply.github.com>
2022-05-18 04:20:14 -0400

    Rearrange widgets

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import widgets from './components/relatedItems&comparison/relatedItems&comparison.jsx';
-import {Overview, Related, Questions, Rating} from './widgets.jsx';
+import { Overview, Reviews, Questions, Related } from './widgets.jsx';
 
-
-//add App component
 class App extends React.Component{
   constructor(props){
     super(props);
@@ -13,6 +11,7 @@ class App extends React.Component{
       currentProductId: 71702
     }
   }
+
   changeCurrentProductId(id) {
     console.log(id);
     this.setState({
@@ -20,19 +19,17 @@ class App extends React.Component{
     })
     //console.log(this.state.currentProductId);
   }
+
   render() {
     return (
       <div>
         <Overview currentProductId={this.state.currentProductId}/>
-        <Related currentProductId={this.state.currentProductId} changeCurrentProductId={this.changeCurrentProductId.bind(this)}/>
+        <Reviews currentProductId={this.state.currentProductId}/>
         <Questions currentProductId={this.state.currentProductId}/>
-        <Rating currentProductId={this.state.currentProductId}/>
+        <Related currentProductId={this.state.currentProductId} changeCurrentProductId={this.changeCurrentProductId.bind(this)}/>
       </div>
     )
   }
 }
 
-
 ReactDOM.createRoot(document.getElementById('app')).render(<App />);
-
-
