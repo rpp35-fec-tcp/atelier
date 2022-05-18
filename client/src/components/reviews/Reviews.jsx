ba47ee8fdea81629618b7e8415209e73ea8b5d6a
kinyipchien <56460213+kinyipchien@users.noreply.github.com>
2022-05-18 04:18:09 -0400

    Rename `ratings&reviews` -> `reviews`

---
+++ client/src/components/reviews/Reviews.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Reviews extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ratings & Reviews</h1>
+      </div>
+    );
+  }
+};
