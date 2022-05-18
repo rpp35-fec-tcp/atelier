070df2155be5ea4b9e0c92d2c36f19c3e6ba242c
kinyipchien <56460213+kinyipchien@users.noreply.github.com>
2022-05-18 04:06:33 -0400

    Refactor `server/index.js`

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(path.join(__dirname, '/../client/dist')));
 app.use(express.json())
 
 const overviewRoute = require('./routes/overview.js');
