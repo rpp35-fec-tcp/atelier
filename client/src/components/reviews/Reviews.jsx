b27f71418e10494b3f95f2f8df5d58c8b4c11879
kinyipchien <56460213+kinyipchien@users.noreply.github.com>
2022-05-20 00:30:45 -0400

    Refactor Reviews to use hooks

--- client/src/components/reviews/Reviews.jsx
+++ client/src/components/reviews/Reviews.jsx
@@ -1,59 +1,44 @@
-import React from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import Review from './Review.jsx';
 import './Reviews.css'
 
-class Reviews extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      displayed: 2,
-      reviewCount: 0,
-      reviews: [],
-    };
-    this.fetchData = this.fetchData.bind(this);
-  }
+const Reviews = (props) => {
+  const [displayed, setDisplayed] = useState(2);
+  const [reviewCount, setReviewCount] = useState(0);
+  const [reviews, setReviews] = useState([]);
 
-  fetchData(product_id) {
-    fetch(`/reviews/?product_id=${product_id}`)
-    .then((response) => response.json())
-    .then((results) => {
-      this.setState({
-        reviewCount: results.length,
-        reviews: results,
-      });
-    })
-  }
+  const fetchData = useCallback(
+    async(product_id) => {
+      const response = await fetch(`/reviews/?product_id=${product_id}`);
+      const results = await response.json();
+      setReviewCount(results.length);
+      setReviews(results);
+    },
+    []
+  );
 
-  componentDidMount() {
-    this.fetchData(this.props.currentProductId);
-  }
+  useEffect(() => {
+    fetchData(props.currentProductId);
+  }, [props.currentProductId]);
 
-  componentDidUpdate(prevProps) {
-    if (this.props.currentProductId !== prevProps.currentProductId) {
-      this.fetchData(this.props.currentProductId);
-    }
-  }
-
-  render() {
-    return (
-      <div className='reviews'>
-        <div className='reviews header'>
-          <h6>RATINGS & REVIEWS</h6>
+  return (
+    <div className='reviews'>
+      <div className='reviews header'>
+        <h6>RATINGS & REVIEWS</h6>
+      </div>
+      <div className='reviews row'>
+        <div className='reviews column left'>
+          &lt;left-column&gt;
         </div>
-        <div className='reviews row'>
-          <div className='reviews column left'>
-            &lt;left-column&gt;
-          </div>
-          <div className='reviews column right'>
-            {this.state.reviews.slice(0, this.state.displayed).map((review) => {
-              return <Review review={review} key={review.review_id}/>
-            })}
-            {this.state.reviewCount > 2 ? <button>MORE REVIEWS</button> : null}
-          </div>
+        <div className='reviews column right'>
+          {reviews.slice(0, displayed).map((review) => {
+            return <Review review={review} key={review.review_id}/>
+          })}
+          {reviewCount > 2 ? <button>MORE REVIEWS</button> : null}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default Reviews;
