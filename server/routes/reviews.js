304f2289465d72481de0dcb5f1006824f7b6d403
kinyipchien <56460213+kinyipchien@users.noreply.github.com>
2022-05-22 03:41:07 -0400

    Refactor route handlers to use async functions and axios instance

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -2,21 +2,36 @@ const axios = require('axios');
 const express = require('express');
 const { TOKEN: AUTH_TOKEN } = require('../../config.js');
 
-const instance = axios.create();
-const CAMPUS = 'hr-rpp';
-instance.defaults.baseURL = `https://app-hrsei-api.herokuapp.com/api/fec2/${CAMPUS}`;
-instance.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 const router = express.Router();
+const CAMPUS = 'hr-rpp';
+const instance = axios.create({
+  baseURL: `https://app-hrsei-api.herokuapp.com/api/fec2/${CAMPUS}/reviews`,
+  timeout: 4000,
+  headers: { Authorization: AUTH_TOKEN }
+});
 
-router.get('/', (req, res) => {
-  instance
-    .get('/reviews', { params: { ...req.query } })
-    .then(({ data }) => {
-      res.send(data.results);
-    })
-    .catch(({ response }) => {
-      res.status(response.status).send(response.statusText);
-    });
+router.get('/', async (req, res) => {
+  try {
+    const { data } = await instance.get(
+      '/',
+      { params: { ...req.query } }
+    );
+    res.send(data.results);
+  } catch({ response }) {
+    res.status(response.status).send(response.statusText);
+  }
+});
+
+router.get('/meta', async (req, res) => {
+  try {
+    const { data } = await instance.get(
+      '/meta',
+      { params: { ...req.query } }
+    );
+    res.send(data);
+  } catch({ response }) {
+    res.status(response.status).send(response.statusText);
+  }
 });
 
 module.exports = router;
