b6430005fbd3fe3ddb6ec920083bd204b6f0faa9
kinyipchien <56460213+kinyipchien@users.noreply.github.com>
2022-05-22 03:52:48 -0400

    Add CharacteristicBar

---
+++ client/src/components/reviews/CharacteristicBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './CharacteristicBar.jsx';
+
+const CharacteristicBar = (props) => (
+  <div className='characteristic-bar'>
+    {props.value}
+  </div>
+);
+
+export default CharacteristicBar;
