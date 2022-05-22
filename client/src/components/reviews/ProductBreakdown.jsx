c82025a59751d7be959a935ec027bebaa5693a8f
kinyipchien <56460213+kinyipchien@users.noreply.github.com>
2022-05-22 03:47:50 -0400

    Add ProductBreakdown

---
+++ client/src/components/reviews/ProductBreakdown.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import CharacteristicBar from './CharacteristicBar.jsx';
+import './ProductBreakdown.css';
+
+const ProductBreakdown = (props) => (
+  <div className='product-breakdown'>
+    {['Size', 'Width', 'Comfort', 'Quality', 'Length', 'Fit'].map(
+      (characteristic) => (
+        props.characteristics[characteristic]
+        && typeof props.characteristics[characteristic].value === 'string'
+        ? <div
+            className='characteristic'
+            key={props.characteristics[characteristic].id}
+          >
+            <div>{characteristic}</div>
+            <CharacteristicBar
+              characteristic={characteristic}
+              value={Number(props.characteristics[characteristic].value)}
+            />
+          </div>
+        : null)
+    )}
+  </div>
+);
+
+export default ProductBreakdown;
