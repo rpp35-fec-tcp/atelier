c4d36c757d037bba2f031c8b4e31445e00dd82db
kinyipchien <56460213+kinyipchien@users.noreply.github.com>
2022-05-25 02:54:12 -0400

    Complete StarRatings and refactor according to Airbnb style guide

--- client/src/components/reviews/StarRatings.jsx
+++ client/src/components/reviews/StarRatings.jsx
@@ -1,10 +1,40 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import './StarRatings.css';
 
-const StarRatings = (props) => (
-  <div className='star-ratings'>
-    {props.rating.toFixed(2)} stars out of {props.numberOfStars}
-  </div>
-);
+function StarRatings({ numberOfStars, precision, rating }) {
+  const roundToNearestQuarter = (value) => (
+    Math.trunc(value) + Math.round((value % 1) / precision) * precision
+  );
+
+  const ratingPercent = (roundToNearestQuarter(rating) / numberOfStars) * 100;
+
+  return (
+    <div className="star-ratings">
+      <div
+        className="stars-empty"
+        data-content={'\u2606'.repeat(numberOfStars)}
+      >
+        <div
+          className="stars-filled"
+          data-content={'\u2605'.repeat(numberOfStars)}
+          style={{ width: `${ratingPercent}%` }}
+        />
+      </div>
+    </div>
+  );
+}
+
+StarRatings.defaultProps = {
+  numberOfStars: 5,
+  precision: 0.25,
+  rating: null,
+};
+
+StarRatings.propTypes = {
+  numberOfStars: PropTypes.number,
+  precision: PropTypes.number,
+  rating: PropTypes.number,
+};
 
 export default StarRatings;
