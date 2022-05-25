a900ca820c7455da2b8ae82aee97169c5936e2e5
kinyipchien <56460213+kinyipchien@users.noreply.github.com>
2022-05-24 20:01:26 -0400

    Attempt to configure polyfills to solve webpack compilation errors

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,16 @@ const config = {
   },
   resolve: {
     extensions: ['.js', '.jsx'],
+    fallback: {
+      'http': require.resolve('stream-http'),
+      'https': require.resolve('https-browserify'),
+      'stream': require.resolve('stream-browserify'),
+      'zlib': require.resolve('browserify-zlib'),
+    }
   },
+  stats: {
+    errorDetails: true,
+  }
 };
 
 module.exports = (env, argv) => {
