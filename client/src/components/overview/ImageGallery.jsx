74bd9e34504ae85cf187cd9f8ca04dd4bdba0687
chloe <312864100@qq.com>
2022-05-27 12:22:13 -0400

    changed image gallery button

--- client/src/components/overview/ImageGallery.jsx
+++ client/src/components/overview/ImageGallery.jsx
@@ -28,7 +28,7 @@ const ThumbnailsView = styled.div`
 `;
 
 const ThumbnailsWrapper = styled.div`
-  margin-top:  ${props => props.offset}px;
+  margin-top: ${(props) => props.offset}px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -82,7 +82,7 @@ const Thumbnail = styled.div`
   }
   &:hover {
     opacity: 0.7;
-    cursor: pointer
+    cursor: pointer;
   }
   &:active {
     opacity: 0.5;
@@ -91,8 +91,52 @@ const Thumbnail = styled.div`
 
 const thumbnailHeight = 48;
 const thumbnailBottom = 18;
-const countVisibleThumbnails = 7
-const thumbnailsHeight = countVisibleThumbnails * (thumbnailHeight + thumbnailBottom);
+const countVisibleThumbnails = 7;
+const thumbnailsHeight =
+  countVisibleThumbnails * (thumbnailHeight + thumbnailBottom);
+
+const ImageButton = styled.div`
+  &{
+    border-radius: 4px;
+    width: 40px;
+    height: 40px;
+    background: rgba(255, 255, 255);
+    border: var(--color-grey-100) solid 2px;];
+    color: var(--color-grey-100);
+    margin-top: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  &:hover {
+    cursor: pointer;
+    border-color: var(--color-grey-300);
+    color: var(--color-grey-300);
+  }
+  &:active {
+    background-color: var(--color-brand-100);
+    border-color: var(--color-brand-400);
+    color: var(--color-brand-400);
+  }
+`;
+
+const UpArrow = styled(MdOutlineExpandLess)`
+  height: 22px;
+  width: 22px;
+`;
+
+const ButtonPlaceholder = styled.div`
+  width: 40px;
+  height: 40px;
+  background: #fff;
+  margin-top: 8px;
+  display: flex;
+`;
+
+const DownArrow = styled(MdOutlineExpandMore)`
+  height: 22px;
+  width: 22px;
+`;
 
 class ImageGallery extends React.Component {
   constructor(props) {
@@ -100,34 +144,75 @@ class ImageGallery extends React.Component {
     this.state = {
       currentIndex: 0,
     };
+    this.imageBack = this.imageBack.bind(this);
+    this.imageForward = this.imageForward.bind(this);
   }
+
+  imageBack(event, expandedView = false) {
+    event.stopPropagation();
+    if (
+      this.state.currentIndex > 0 &&
+      (event.key === undefined || event.key === 'Enter' || expandedView)
+    ) {
+      this.setState((prevState) => ({
+        currentIndex: prevState.currentIndex - 1,
+      }));
+    }
+  }
+
+  imageForward(event, expandedView = false) {
+    event.stopPropagation();
+    if (
+      this.state.currentIndex < this.props.photos.length - 1 &&
+      (event.key === undefined || event.key === 'Enter' || expandedView)
+    ) {
+      this.setState((prevState) => ({
+        currentIndex: prevState.currentIndex + 1,
+      }));
+    }
+  }
+
   render() {
-    const {currentStyle, photos} = this.props;
+    const { currentStyle, photos } = this.props;
 
     const thumbnails = photos.map((photo, index) => {
-      return (
-      index === this.state.currentIndex ? (
-      <ThumbnailSelected key={index}>
-        {photo.thumbnail_url ?
-          (<ThumbnailWrapper url={photo.thumbnail_url} value={index} />) :
-          (<ThumbnailEmpty selected={true} ><MdOutlineHideImage/></ThumbnailEmpty>)
-        }
-        <ThumbnailSelectedIndicator/>
-      </ThumbnailSelected>
+      return index === this.state.currentIndex ? (
+        <ThumbnailSelected key={index}>
+          {photo.thumbnail_url ? (
+            <ThumbnailWrapper url={photo.thumbnail_url} value={index} />
+          ) : (
+            <ThumbnailEmpty selected={true}>
+              <MdOutlineHideImage />
+            </ThumbnailEmpty>
+          )}
+          <ThumbnailSelectedIndicator />
+        </ThumbnailSelected>
       ) : (
-      <Thumbnail key={index} >
-      {photo.thumbnail_url ?
-        (<ThumbnailWrapper tabIndex="0" onKeyPress={this.changeImage} onClick={this.changeImage} url={photo.thumbnail_url} value={index} />) :
-        (<ThumbnailEmpty selected={false} ><MdOutlineHideImage/></ThumbnailEmpty>)
-      }
-      </Thumbnail>
-      )
-    )});
+        <Thumbnail key={index}>
+          {photo.thumbnail_url ? (
+            <ThumbnailWrapper
+              tabIndex='0'
+              onKeyPress={this.changeImage}
+              onClick={this.changeImage}
+              url={photo.thumbnail_url}
+              value={index}
+            />
+          ) : (
+            <ThumbnailEmpty selected={false}>
+              <MdOutlineHideImage />
+            </ThumbnailEmpty>
+          )}
+        </Thumbnail>
+      );
+    });
 
-    const thumbnailsOffset = this.state.currentIndex >= countVisibleThumbnails ? -(this.state.currentIndex - countVisibleThumbnails + 1) * (thumbnailHeight + thumbnailBottom) : 0;
+    const thumbnailsOffset =
+      this.state.currentIndex >= countVisibleThumbnails
+        ? -(this.state.currentIndex - countVisibleThumbnails + 1) *
+          (thumbnailHeight + thumbnailBottom)
+        : 0;
 
     let nextImage, previousImage, nextThumbnail, previousThumbnail;
-
 
     return (
       <Gallery>
@@ -135,21 +220,26 @@ class ImageGallery extends React.Component {
           <ThumbnailsView>
             <ThumbnailsWrapper>{thumbnails}</ThumbnailsWrapper>
           </ThumbnailsView>
-          <button
-            className='image-gallery-buttons'
-            id='back'
-            onClick={this.props.changePhoto}
-          >
-            ←
-          </button>
-          <img src={this.props.photoURL} />
-          <button
-            class='image-gallery-buttons'
-            id='forward'
-            onClick={this.props.changePhoto}
-          >
-            →
-          </button>
+          {this.state.currentIndex > 0 ? (
+            <ImageButton
+              onClick={this.imageBack}
+              onKeyPress={this.imageBack}
+              tabIndex='0'
+            >
+              <UpArrow />
+            </ImageButton>
+          ) : (
+            <ButtonPlaceholder></ButtonPlaceholder>
+          )}
+          {this.state.currentIndex < photos.length - 1 && (
+            <ImageButton
+              onClick={this.imageForward}
+              onKeyPress={this.imageForward}
+              tabIndex='0'
+            >
+              <DownArrow />
+            </ImageButton>
+          )}
         </ThumbnailsSection>
       </Gallery>
     );
