5df3e1373ac97ef0faae8dde7391ee1f12c6ef9f
ysyanshen <ysyanshen1987@gmail.com>
2022-05-28 12:59:56 -0700

    wrong

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import { Overview, Related, Questions } from './widgets.jsx';
 import Reviews from './components/reviews/Reviews.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    console.log(window.location.pathname)
     this.state = {
-      //currentProductId: Number(window.location.pathname.slice(1))
-      currentProductId: 71697
+      currentProductId: Number(window.location.pathname.slice(1))
+      //currentProductId: 71697
     };
     this.changeCurrentProductId = this.changeCurrentProductId.bind(this);
     console.log('this.state.currentProductId: ', this.state.currentProductId);
@@ -21,6 +23,7 @@ class App extends React.Component {
   }
 
   render() {
+    console.log(window.location.pathname)
     return (
       <div>
         <Overview currentProductId={this.state.currentProductId}  changeCurrentProductId={this.changeCurrentProductId.bind(this)}/>
@@ -34,6 +37,29 @@ class App extends React.Component {
     );
   }
 }
+// function App() {
+//     var currentProductId = 71697;
+
+//     return (
+//       <div>
+//       <Link to="/id">ID</Link>
+//         <Overview currentProductId={currentProductId} />
+//         {/* <Related
+//           currentProductId={this.state.currentProductId}
+//         /> */}
+//         <Questions currentProductId={currentProductId} />
+//         <Reviews currentProductId={currentProductId} />
+//       </div>
+//     );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+// root.render(
+//   <BrowserRouter>
+//     <Routes>
+//       <Route path='/' element={<App />} />
+//         <Route path='id' element={<App />} />
+//     </Routes>
+//   </BrowserRouter>
+// );
