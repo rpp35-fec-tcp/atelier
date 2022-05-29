30039460d0c1e455f956539bd029230d97c556fd
ysyanshen <ysyanshen1987@gmail.com>
2022-05-29 10:40:02 -0700

    solve merge issue

--- client/src/widgets.jsx
+++ client/src/widgets.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable max-classes-per-file */
 import React from 'react';
+import axios from 'axios';
 import RelatedComponent from './components/relatedItems&comparison/relatedComponent';
 import QuestionList from './components/questions&answers/listQuestions';
 import AppOverview from './components/overview/AppOverview';
@@ -29,12 +30,34 @@ export class Questions extends React.Component {
     this.handleInteraction = this.handleInteraction.bind(this);
   }
 
+  handleInteraction (element, widget) {
+    const date = new Date();
+    let time = date.getTime(); //returns UTC time stamp;
+    time = time.toString();
+
+    axios.post('http://localhost:3000/interactions', {
+      data: {
+        element,
+        widget,
+        time,
+      },
+    })
+      .then((res) => {
+        console.log('interaction status', res.status);
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
+  }
+
   render() {
     return (
       <div>
         <h1>Question</h1>
-        <QuestionList product_id={this.props.currentProductId}
-        handleInteraction={this.handleInteraction}/>
+        <QuestionList
+          product_id={this.props.currentProductId}
+          handleInteraction={this.handleInteraction}
+        />
       </div>
     );
   }
